2906e3baaeb7a39e554648f952ca2bccfee14551
Amogh Mantri <amogh.mantri@yahoo.com>
2023-02-16 16:19:42 -0500

    INIT

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -5,17 +5,26 @@
 //  Created by Amogh Mantri on 2/16/23.
 //
 
+//Goal: Create a app that displays how much money each person has to pay with a given bill amount and amount of people
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+      
+      //TODO: Add a stack to organize views
+      
+      //TODO: Add a textfield for the bill amount
+      
+      //TODO: Add a slider for the amount of people present
+      
+      //TODO: Add Text that displays the amount each person needs to pay
+      
+      //TODO: Add "money image" on top of screen
+      
+      //TODO: Change background to a light blue
+        Text("Bill Split")
+        
     }
 }
 
