65b68a58e42f35609752d9fbd30361df80285687
Amogh Mantri <amogh.mantri@yahoo.com>
2023-02-16 15:59:22 -0500

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Amogh Mantri on 2/16/23.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
